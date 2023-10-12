84211881ee605714aea4e2a520e3b4bee28d86dd
sllachod <sllachod@unsa.edu.pe>
2023-10-12 18:15:14 -0500

    reduciendo las import con *

--- fase02/Laboratorio06/VideoJuego3.java
+++ fase02/Laboratorio06/VideoJuego3.java
@@ -1,8 +1,4 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class VideoJuego3 {
     private static final int TABLERO_SIZE = 5;
