8f3a3a9e90b255c5fcb42107108a7fc6b35dd586
sllachod <sllachod@unsa.edu.pe>
2023-12-17 21:24:50 -0500

    se imprimen los datos de cada soldado

--- fase02/Laboratorio06/VideoJuego3.java
+++ fase02/Laboratorio06/VideoJuego3.java
@@ -5,8 +5,11 @@ public class VideoJuego3{
         ArrayList<ArrayList<Soldado>> tablero = new ArrayList<>();
         ArrayList<Soldado> ejercito1 = new ArrayList<>();
         ArrayList<Soldado> ejercito2 = new ArrayList<>();
+
         inicializarTablero(tablero, ejercito1, ejercito2, n);
+        
         imprimirTablero(tablero);
+        imprimirDatosSoldados(ejercito1, ejercito2);
     }
 
     private static void inicializarTablero(ArrayList<ArrayList<Soldado>> tablero, ArrayList<Soldado> ejercito1, ArrayList<Soldado> ejercito2, int n) {
@@ -18,11 +21,11 @@ public class VideoJuego3{
 
         for (int i = 0; i < 10; i++) {
             // Crear soldado para el ejército 1
-            Soldado soldado1 = crearSoldado(i, 1, random, tablero);
+            Soldado soldado1 = crearSoldados(i, 1, random, tablero);
             ejercito1.add(soldado1);
 
             // Crear soldado para el ejército 2
-            Soldado soldado2 = crearSoldado(i, 2, random, tablero);
+            Soldado soldado2 = crearSoldados(i, 2, random, tablero);
             ejercito2.add(soldado2);
         }
     }
@@ -36,7 +39,7 @@ public class VideoJuego3{
         }
         System.out.println();
     }
-    private static Soldado crearSoldado(int index, int ejercito, Random random, ArrayList<ArrayList<Soldado>> tablero) {
+    private static Soldado crearSoldados(int index, int ejercito, Random random, ArrayList<ArrayList<Soldado>> tablero) {
         String nombre = "Soldado" + index + "X" + ejercito;
         int puntosVida = random.nextInt(5) + 1;
         int fila, columna;
@@ -53,6 +56,18 @@ public class VideoJuego3{
     }
     private static boolean yaOcupado(ArrayList<ArrayList<Soldado>> tablero, int fila, int columna) {
         return !tablero.get(fila).isEmpty() && tablero.get(fila).get(tablero.get(fila).size() - 1).getColumna() == columna;
+    }
+    private static void imprimirDatosSoldados(ArrayList<Soldado> ejercito1, ArrayList<Soldado> ejercito2) {
+        System.out.println("Datos de los soldados del Ejército 1:");
+        for (Soldado soldado : ejercito1) {
+            System.out.println(soldado.getNombre() + " - Puntos de Vida: " + soldado.getPuntosVida());
+        }
+        System.out.println();
+        System.out.println("Datos de los soldados del Ejército 2:");
+        for (Soldado soldado : ejercito2) {
+            System.out.println(soldado.getNombre() + " - Puntos de Vida: " + soldado.getPuntosVida());
+        }
+        System.out.println();
     }
 }
     
