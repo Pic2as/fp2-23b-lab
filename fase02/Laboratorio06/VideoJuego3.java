b9df6f05adb5aad572d4a42c35932834a6a74614
sllachod <sllachod@unsa.edu.pe>
2023-12-17 21:10:22 -0500

    creando arraylist de cada ejercito y tablero

--- fase02/Laboratorio06/VideoJuego3.java
+++ fase02/Laboratorio06/VideoJuego3.java
@@ -1,21 +1,10 @@
 import java.util.*;
 public class VideoJuego3{
     public static void main(String[]args){
-        int filas = 10;
-        int columnas = 10;
-        ArrayList<ArrayList<String>> tablero;
-        private static voidtablero(int filas, int columnas)
-        {
-            tablero = new ArrayList<>();
-            for (int i = 0; i < filas; i++)
-            {
-                ArrayList<String> fila = new ArrayList<>();
-                for(int j = 0; j < columnas; j++) {
-                    fila.add("   ");
-                }
-                tablero.add(fila);
-            }
-        }
+        int n = 10; // Número de soldados por ejército
+        ArrayList<ArrayList<Soldado>> tablero = new ArrayList<>();
+        ArrayList<Soldado> ejercito1 = new ArrayList<>();
+        ArrayList<Soldado> ejercito2 = new ArrayList<>();
     }
 }
     
