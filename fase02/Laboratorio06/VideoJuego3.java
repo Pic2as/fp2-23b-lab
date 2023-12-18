074626b30efabbb2ddce34271c7c983d2056d1e1
sllachod <sllachod@unsa.edu.pe>
2023-12-17 21:14:58 -0500

    creando 3 funciones, para inicializar tablero, crear soldado y verificar si ya esta ocupado

--- fase02/Laboratorio06/VideoJuego3.java
+++ fase02/Laboratorio06/VideoJuego3.java
@@ -6,5 +6,40 @@ public class VideoJuego3{
         ArrayList<Soldado> ejercito1 = new ArrayList<>();
         ArrayList<Soldado> ejercito2 = new ArrayList<>();
     }
+    private static void inicializarTablero(ArrayList<ArrayList<Soldado>> tablero, ArrayList<Soldado> ejercito1, ArrayList<Soldado> ejercito2, int n) {
+        Random random = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            tablero.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < n; i++) {
+            // Crear soldado para el ejército 1
+            Soldado soldado1 = crearSoldado(i, 1, random, tablero);
+            ejercito1.add(soldado1);
+
+            // Crear soldado para el ejército 2
+            Soldado soldado2 = crearSoldado(i, 2, random, tablero);
+            ejercito2.add(soldado2);
+        }
+    }
+    private static Soldado crearSoldado(int index, int ejercito, Random random, ArrayList<ArrayList<Soldado>> tablero) {
+        String nombre = "Soldado" + index + "X" + ejercito;
+        int puntosVida = random.nextInt(5) + 1;
+        int fila, columna;
+
+        do {
+            fila = random.nextInt(10);
+            columna = random.nextInt(10);
+        } while (yaOcupado(tablero, fila, columna));
+
+        Soldado soldado = new Soldado(nombre, puntosVida, fila, columna);
+        tablero.get(fila).add(soldado);
+
+        return soldado;
+    }
+    private static boolean yaOcupado(ArrayList<ArrayList<Soldado>> tablero, int fila, int columna) {
+        return !tablero.get(fila).isEmpty() && tablero.get(fila).get(tablero.get(fila).size() - 1).getColumna() == columna;
+    }
 }
     
