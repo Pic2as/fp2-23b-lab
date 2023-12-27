8135a3e28968fb7333b2ec7597782a58e07a7f9d
sllachod <sllachod@unsa.edu.pe>
2023-12-27 17:53:39 -0500

    creando metodo sobrecargado

--- Laboratorio09/Soldado.java
+++ Laboratorio09/Soldado.java
@@ -7,4 +7,29 @@ public class Soldado {
     private int velocidad;
     private String actitud;
     private boolean vive;
+
+    public Soldado(String nombre, int nivelVida) {
+        this.nombre = nombre;
+        this.nivelVida = nivelVida;
+        this.vidaActual = nivelVida;
+        this.velocidad = 0;
+        this.actitud = "Defensiva";
+        this.vive = true;
+    }
+
+    public Soldado(String nombre, int nivelAtaque, int nivelDefensa, int nivelVida, int velocidad, String actitud) {
+        this.nombre = nombre;
+        this.nivelAtaque = nivelAtaque;
+        this.nivelDefensa = nivelDefensa;
+        this.nivelVida = nivelVida;
+        this.vidaActual = nivelVida;
+        this.velocidad = velocidad;
+        this.actitud = actitud;
+        this.vive = true;
+    }
+
+    public Soldado(String nombre, int nivelAtaque, int nivelDefensa, int nivelVida, int velocidad, String actitud, boolean vive) {
+        this(nombre, nivelAtaque, nivelDefensa, nivelVida, velocidad, actitud);
+        this.vive = vive;
+    }
 }
