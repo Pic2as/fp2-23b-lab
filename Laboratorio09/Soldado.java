1e299826930493eb2faff8459fd569c11bf3c42b
sllachod <sllachod@unsa.edu.pe>
2023-12-27 17:30:58 -0500

    creando variables en clase soldado

---
+++ Laboratorio09/Soldado.java
@@ -0,0 +1,10 @@
+public class Soldado {
+    private String nombre;
+    private int nivelAtaque;
+    private int nivelDefensa;
+    private int nivelVida;
+    private int vidaActual;
+    private int velocidad;
+    private String actitud;
+    private boolean vive;
+}
