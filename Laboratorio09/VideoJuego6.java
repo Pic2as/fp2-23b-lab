89fae8415fa0b39d15d8dbaa41b58bbedbaa7ef8
sllachod <sllachod@unsa.edu.pe>
2023-12-28 14:07:36 -0500

    realizando cambios

--- Laboratorio09/VideoJuego6.java
+++ Laboratorio09/VideoJuego6.java
@@ -1,14 +1,23 @@
+import java.util.Random;
 
 public class VideoJuego6 {
     public static void main(String[] args) {
         int filas = 10;
         int columnas = 10;
+        String[] soldados = new String[2];
         String[][] tablero = new String[filas][columnas];
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < columnas; j++) {
-                tablero[i][j] = "|_| ";
+                tablero[i][j] = "|-| ";
             }
         }
+        Random random = new Random();
+        int n = random.nextInt(10) + 1;
+        crearSoldadosA(n, soldados);
+    }
+    public static String[] crearSoldadosA(int n, String[] soldados){
 
+
+        return ;
     }
 }
