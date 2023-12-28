8307750ce6d12d14d83dea663a6de8d3734b5da8
sllachod <sllachod@unsa.edu.pe>
2023-12-28 14:14:47 -0500

    creando soldados A

--- Laboratorio09/VideoJuego6.java
+++ Laboratorio09/VideoJuego6.java
@@ -1,10 +1,10 @@
-import java.util.Random;
+import java.util.*;
 
 public class VideoJuego6 {
     public static void main(String[] args) {
         int filas = 10;
         int columnas = 10;
-        String[] soldados = new String[2];
+        
         String[][] tablero = new String[filas][columnas];
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < columnas; j++) {
@@ -13,11 +13,26 @@ public class VideoJuego6 {
         }
         Random random = new Random();
         int n = random.nextInt(10) + 1;
-        crearSoldadosA(n, soldados);
+        Soldado[] soldados = new Soldado[n];
+        soldados = crearSoldadosA(n);
     }
-    public static String[] crearSoldadosA(int n, String[] soldados){
+    public static Soldado[] crearSoldadosA(int n){
+        Random random = new Random();
+        Soldado[] ejercito = new Soldado[n];
 
+        for (int i = 0; i < n; i++) {
+            String nombre = "Soldado" + i;
+            int nivelAtaque = random.nextInt(5) + 1;
+            int nivelDefensa = random.nextInt(5) + 1;
+            int nivelVida = random.nextInt(5) + 1;
+            int vidaActual = nivelVida;
+            int velocidad = random.nextInt(5) + 1;
+            String actitud = "Neutral";
+            boolean vive = true;
 
-        return ;
+            ejercito[i] = new Soldado(nombre, nivelAtaque, nivelDefensa, nivelVida, vidaActual, velocidad, actitud, vive);
+        }
+
+        return ejercito;
     }
 }
